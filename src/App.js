0fb7faee1bffd69a161a9c1de83611987f6cb025
[200~git config --global user.name FIRST_NAME <ferjanikhouloud@gmail.com>
2023-08-29 03:44:30 +0200

    react props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { Card } from 'react-bootstrap';
+import Player from './Components/Player/Player';
 
 function App() {
+  const players = [
+    {
+      Name: "MESSI",
+      Team: "Barcelone",
+      Nationality:"Argentina",
+      JerseyNumber:"9",
+      Age:"33",
+      Image : 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Lionel-Messi-Argentina-2022-FIFA-World-Cup_%28cropped%29.jpg/250px-Lionel-Messi-Argentina-2022-FIFA-World-Cup_%28cropped%29.jpg'
+    },
+    {
+      Name: "Ronaldo",
+      Team: "Real",
+      Nationality:"Portuguese",
+      JerseyNumber:"7",
+      Age:"38",
+      Image : "https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Cristiano_Ronaldo_WC2022_-_01.jpg/250px-Cristiano_Ronaldo_WC2022_-_01.jpg"
+    },
+    {
+      Name: "Davids",
+      Team: "Juvintise",
+      Nationality:"Holland",
+      JerseyNumber:"9",
+      Age:"35",
+      Image:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Edgar_Davids.jpg/250px-Edgar_Davids.jpg"
+    },
+    {
+      Name: "MbappéS",
+      Team: "PSG",
+      Nationality:"France",
+      JerseyNumber:"7",
+      Age:"24",
+      Image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/2019-07-17_SG_Dynamo_Dresden_vs._Paris_Saint-Germain_by_Sandro_Halank%E2%80%93129_%28cropped%29.jpg/220px-2019-07-17_SG_Dynamo_Dresden_vs._Paris_Saint-Germain_by_Sandro_Halank%E2%80%93129_%28cropped%29.jpg"
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ players.map((player,key) => (
+  <Player key={key} name={player.Name} team={player.Team} nationality={player.Nationality} jerseyNumber={player.JerseyNumber} age={player.Age} image={player.Image}/>
+ ))
+  )
+ }
+
 
 export default App;
+
+
+
---
+++ src/Components/Player/Player.js
@@ -0,0 +1,34 @@
+import react from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function Player ({name,team,nationality,jerseyNumber,age,image}) {
+    return (
+        <>
+          
+        
+
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={image} />
+      <Card.Body>
+        <Card.Title>
+      <div>Name:{name}</div></Card.Title>
+        <Card.Text>
+       <div>Team : {team} </div>
+       <div>Nationality: {nationality} </div>
+       <div>JerseyNumber: {jerseyNumber} </div>
+       <div>Age:{age}</div>
+       
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+
+        </>
+
+        
+      
+    );
+}
+
+export default Player; 
